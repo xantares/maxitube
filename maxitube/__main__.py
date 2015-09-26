1cb39adb957d2dfe67c9ed94b46a3de0455dcfab
xantares <xantares09@hotmail.com>
2015-09-26 22:13:42 +0200

    Add wmp

--- maxitube/__main__.py
+++ maxitube/__main__.py
@@ -487,10 +487,17 @@ class MainWindow(QtGui.QMainWindow):
         if not filename:
             return
 
-        cmd_list = ['vlc', '--fullscreen', filename]
-        cmd_line = subprocess.list2cmdline(cmd_list)
-        print('-- run player:', cmd_line)
-        p = subprocess.Popen(cmd_list)
+        cmd_lists = [['vlc', '--fullscreen', filename]]
+        if sys.platform.startswith('win'):
+            cmd_list.append(['C:\Program Files (x86)\Windows Media Player\wmplayer.exe', '/fullscreen', filename])
+        for cmd_list in cmd_lists:
+            try:
+                cmd_line = subprocess.list2cmdline(cmd_list)
+                print('-- run player:', cmd_line)
+                p = subprocess.Popen(cmd_list)
+            except:
+                print('-- failed to run', cmd_line)
+                pass
 
     def __init__(self):
         super(MainWindow, self).__init__()
